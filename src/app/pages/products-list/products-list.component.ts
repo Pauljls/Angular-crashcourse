9ee02e47416713b76039c2c81441ed150f7aa17c
Pauljls <paulazarsol@gmail.com>
2025-01-04 09:36:20 -0500

    Usando Loaders

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,27 +1,49 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { Product } from '../../models/products.models';
 import { ProductCardComponent } from './product-card/product-card.component';
+
 @Component({
   selector: 'app-products-list',
   standalone: true,
   imports: [ProductCardComponent],
   template: `
     <div class="p-8 grid grid-cols-2 gap-4">
-      @for( product of products(); track product.id){
+    @if (!loading() && products().length !== 0) {
+      @for(product of products(); track product.id) {
         <div>
           <app-product-card [product]="product"/>
         </div>
       }
+    } @else {
+      <div class="flex flex-col items-center justify-center">
+        @if (loading()) {
+          <h1 class="text-2xl"><span class="loading loading-spinner text-accent"></span> Cargando productos...</h1>
+        } @else {
+          <h1 class="text-2xl">Aun no hay items</h1>
+        }
+      </div>
+    }
     </div>
   `,
   styles: ''
 })
-export class ProductsListComponent {
-
-  async ngOnInit(){//USAMOS ESTE HOOK PARA INDICAR QUE ALGO SE EJCUTE DURANTE LA CREACION DEL COMPONENTE
-    const res = await fetch('https://fakestoreapi.com/products/category/electronics')
-    const data = await res.json();
-    this.products.set(data)
-  }
+export class ProductsListComponent implements OnInit {
   products = signal<Product[]>([]);
+  loading = signal<boolean>(true);
+
+  ngOnInit() {
+    this.fetchProducts();
+  }
+
+  private async fetchProducts() {
+    try {
+      const res = await fetch('https://fakestoreapi.com/products/category/electronics');
+      const data = await res.json();
+      this.products.set(data);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    } finally {
+      this.loading.set(false);
+    }
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}"
@@ -6,6 +8,13 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    daisyui,
+  ],
+  daisyui: {
+    themes: ["light"], // Esto establece light como tema único
+    // O si quieres tener múltiples temas pero light por defecto:
+    // themes: true, // Esto habilita todos los temas
+    // defaultTheme: "light", // Esto establece light como tema por defecto
+  }
 }
-
