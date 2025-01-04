c90ea95aecd72c9fa7f4cb6d128a6075e7ebde55
Pauljls <paulazarsol@gmail.com>
2025-01-04 15:56:35 -0500

    Agregando indicadores

--- src/app/components/primary-button/primary-button.component.ts
+++ src/app/components/primary-button/primary-button.component.ts
@@ -5,8 +5,12 @@ import { Component,input, output } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <button (click)="handleButtonClick()" class="bg-blue-500 text-white w-full border px-5 py-2 rounded-xl shadow-md hover:opacity-90">
-      {{label()}}
+    <button (click)="handleButtonClick()" class="bg-blue-500 text-white w-full border px-5 py-2 rounded-xl shadow-md hover:opacity-90 
+    flex flex-row space-x-2">
+     <div>
+     <ng-content selector=[svg]/> 
+     </div>
+     <div>{{label()}}</div>
     </button>
   `,
   styleUrl: './primary-button.component.css'
